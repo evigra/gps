348512751a0db3b2e93fc58ded578bce33e79707
Eduardo Vizcaino <evigra@hotmail.com>
2021-11-07 21:47:04 -0600

    ADD files V12

--- gpsmap/models/models.py
+++ gpsmap/models/models.py
@@ -68,14 +68,13 @@ class tc_devices(models.Model):
     _description = 'traccar devices'
     _order = "name DESC"
         
-    name                                        = fields.Char('Name', size=128)
-    uniqueid                                    = fields.Char('IMEI', size=128)
+    name                                        = fields.Char('Name', size=128, required=True)
+    uniqueid                                    = fields.Char('IMEI', size=128, required=True)
     icc                                         = fields.Char('ICC', size=30)
     phone                                       = fields.Char('Phone', size=128)
     model                                       = fields.Char('Model', size=128)
     lastupdate                                  = fields.Datetime('Lastupdate')
     disabled                                    = fields.Boolean('Disable', default=False)
-    telcel                                      = fields.Boolean('Telcel', default=True)
     signal                                      = fields.Boolean('Good signal', default=True)
     company_ids                                 = fields.Many2many('res.company', 'tcdevices_res_company_rel', 'user_id', 'cid', string='Companies', default=lambda self: self.env.user.company_id)
     
@@ -236,12 +235,11 @@ class vehicle(models.Model):
         for position in positions:
             
             #if(position["status"]=="Offline"):
-            #    print("status==",position["status"]," device==",position["devicetime"]," server===",position["servertime"]," fix==",position["fixtime"])
+            print("status==",position["status"]," device==",position["devicetime"]," server===",position["servertime"]," fix==",position["fixtime"])
             position["de"]            =position["tp_deviceid"]                            
             tp_deviceid               =position["tp_deviceid"]
             
-            return_positions[tp_deviceid]    =position
-            
+            return_positions[tp_deviceid]    =position            
         return return_positions    
     @api.multi
     def positions(self,datas):		   
